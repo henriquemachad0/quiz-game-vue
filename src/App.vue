4530301e91eb1a1c36367f99a19646e8043cc8b7
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-05-23 11:48:29 -0300

    fix: clean components unused

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
-  <HelloWorld msg="Our first vue js app"/>
+  <div>
+
+    <h1>
+    Microphones can be used not only to pick up sound, but also to project sound similar to a speaker.
+    </h1>
+
+    <input type="radio" name="options" value="True">
+    <label>True</label><br>
+    
+    <input type="radio" name="options" value="False">
+    <label>False</label><br>
+    
+    <button class="send" type="button">Confirmar</button>
+    
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
+
+// https://opentdb.com/api.php?amount=1&category=18
+
 </script>
 
 <style lang="scss">
@@ -20,6 +33,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+
+  input[type=radio] {
+    margin: 12px 4px;
+  }
+
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
+  }
 }
 </style>
